8d6fd89a07727cc7ad60e183cf231089b0e2b12e
Juan Sebastian <juan.2114@hotmail.com>
2016-09-22 23:06:28 -0500

    re arranged some strings into constants

--- initdb.py
+++ initdb.py
@@ -5,7 +5,14 @@ from contextlib import closing
 # Base de datos oficial
 #DATABASE = '/home/abetpujc/abetpujc/abet.db'
 # Para hacer pruebas
-DATABASE = '/Users/gsarria/Dropbox/Work/ABETForm/github/abetpujc/abet.db'
+#DATABASE = '/Users/gsarria/Dropbox/Work/ABETForm/github/abetpujc/abet.db'
+DATABASE = 'C:/Users/Usuario/Documents/Javeriana/3 Semestre/ABET/abet.db'
+
+#DATA_DATABASE = 'scripts/addData2db.sql'
+DATA_DATABASE = 'C:/Users/Usuario/Desktop/Abet/abetpujc-master/scripts/addData2db.sql'
+
+#ABET_DATABASE = 'scripts/DB_Abet_SQLite.sql'
+ABET_DATABASE = 'C:/Users/Usuario/Desktop/Abet/abetpujc-master/scripts/DB_Abet_SQLite.sql'
 
 
 def connect_db():
@@ -13,7 +20,7 @@ def connect_db():
 
 
 def add_data(db):
-    with open('scripts/addData2db.sql', mode='r') as f:
+    with open(DATA_DATABASE, encoding='utf8', mode='r') as f:
         print("Filling DB")
         db.executescript(f.read())
     db.commit()
@@ -21,7 +28,7 @@ def add_data(db):
 
 def init_db():
     with closing(connect_db()) as db:
-        with open('scripts/DB_Abet_SQLite.sql', mode='r') as f:
+        with open(ABET_DATABASE, encoding='utf8', mode='r') as f:
             print("Creating DB")
             db.executescript(f.read())
             add_data(db)
